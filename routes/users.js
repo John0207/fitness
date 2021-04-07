495b7a0b9772be9fc01caa1cd214e2272a953818
John0207 <john.deluccia@snhu.edu>
2021-04-07 00:33:00 -0400

    fixed register to also login

--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,11 @@ router.post('/register', catchAsync(async (req, res) => {
     const {email, username, password} = req.body;
     const user = new User({email, username});
     const registeredUser = await User.register(user, password);
+    req.login(registeredUser, err => {
+        if (err) return next(err);
+        req.flash('success', 'Welcome to fitness');
+        res.redirect('/classes');
+    })
     req.flash('success', 'Welcome to fitness');
     res.redirect('/classes');
     } catch (e) {
