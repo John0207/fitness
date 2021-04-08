98f116a99ee6a1e8f0b1b08384d0e2d17597fcd7
John0207 <john.deluccia@snhu.edu>
2021-04-08 16:31:28 -0400

    restructred class routes using router.route

--- routes/classes.js
+++ routes/classes.js
@@ -5,18 +5,19 @@ const classes = require('../controllers/classes');
 const { isLoggedIn } = require('../middleware');
 const { validateClass } = require('../middleware');
 
-router.get('/', catchAsync(classes.index));
+router.route('/')
+    .get(catchAsync(classes.index))
+    .post(isLoggedIn, validateClass, catchAsync(classes.createClass))
+
+
 
 router.get('/new', isLoggedIn, classes.renderNewForm)
 
-router.post('/', isLoggedIn, validateClass, catchAsync(classes.createClass))
-
-router.get('/:id', catchAsync(classes.showClass));
+router.route('/:id')
+    .get(catchAsync(classes.showClass))
+    .put(isLoggedIn, catchAsync(classes.updateClass))
+    .delete(isLoggedIn, catchAsync(classes.deleteClass))
 
 router.get('/:id/edit', isLoggedIn, catchAsync(classes.renderEditForm))
-
-router.put('/:id', isLoggedIn, catchAsync(classes.updateClass));
-
-router.delete('/:id', isLoggedIn, catchAsync(classes.deleteClass));
 
 module.exports = router;
